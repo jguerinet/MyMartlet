f72060739800445ea87db36ada0ba7a9f61a0fd0
jguerinet <julien@guerinet.com>
2014-01-22 21:22:01 -0500

    Set up design for DesktopActivity, put a basic URL in webview.

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/DesktopActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/DesktopActivity.java
@@ -1,7 +1,10 @@
 package ca.mcgill.mymcgill.activity;
 
 import android.app.Activity;
-import android.os.Bundle;import ca.mcgill.mymcgill.R;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import ca.mcgill.mymcgill.R;
 
 /**
  * Author: Julien
@@ -11,5 +14,11 @@ public class DesktopActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desktop);
+
+        //Get the Webview
+        WebView webView = (WebView)findViewById(R.id.desktop_webview);
+
+        //TODO Load the right URL here
+        webView.loadUrl("www.mymcgill.mcgill.ca");
     }
 }
