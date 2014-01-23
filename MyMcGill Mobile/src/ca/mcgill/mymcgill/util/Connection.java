2917bb1f753cfc9d26dfaa76bff43c32e5b2207d
jguerinet <julien@guerinet.com>
2014-01-22 20:10:47 -0500

    Added skeleton classes in the Utilities package

---
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Connection.java
@@ -0,0 +1,9 @@
+package ca.mcgill.mymcgill.util;
+
+/**
+ * Author: Julien
+ * Date: 22/01/14, 8:09 PM
+ * This package will hold the logic for logging someone in to MyMcGill
+ */
+public class Connection {
+}
