cc4c20828943c495f4ddc87230d89d1827bd0121
jguerinet <julien@guerinet.com>
2014-01-22 21:49:05 -0500

    Worked on Logic in Splash screen: - First checks if there are stored credentials: if not, it sends the user to the LoginActivity - Tries to connect wth credentials, success go to MainActivity, error go to LoginActivity with error message

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/SplashActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/SplashActivity.java
@@ -2,9 +2,13 @@ package ca.mcgill.mymcgill.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import ca.mcgill.mymcgill.R;
+import ca.mcgill.mymcgill.util.Connection;
+import ca.mcgill.mymcgill.util.Constants;
 
 /**
  * Author: Julien
@@ -15,8 +19,33 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //TODO TEMPORARY
-        startActivity(new Intent(this, LoginActivity.class));
+        //Get the SharedPreferences
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
+        //Get the username and password stored
+        String username = sharedPrefs.getString(Constants.USERNAME, null);
+        String password = sharedPrefs.getString(Constants.PASSWORD, null);
+
+        //If one of them is null, send the user to the LoginActivity
+        if(username == null || password == null){
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
+        //If not, try connecting
+        int connectionStatus = Connection.connect(this, username, password);
+
+        //Successful connection : go to MainActivity
+        if(connectionStatus == Constants.CONNECTION_OK){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        else{
+            //Unsuccessful connection : go to LoginActivity with error message
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtra(Constants.CONNECTION_STATUS, connectionStatus);
+            startActivity(intent);
+            finish();
+        }
     }
 }
