eeb8433e7a255f9b99fdcca0c2619dd79dd30c70
jguerinet <julien@guerinet.com>
2014-01-22 21:04:27 -0500

    Wired the views to the methods.

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/MainActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/MainActivity.java
@@ -3,6 +3,7 @@ package ca.mcgill.mymcgill.activity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 import ca.mcgill.mymcgill.R;
 
@@ -14,6 +15,20 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
+    //This method is called when the desktop button is clicked
+    public void viewDesktopSite(View v){
+
+    }
+
+    //This method is called when the schedule button is clicked
+    public void viewSchedule(View v){
+
+    }
+
+    //This method is called when the logout button is clicked
+    public void logout(View v){
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
