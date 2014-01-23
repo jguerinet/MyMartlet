cd5c60c12114dc4e197c2bd51f758adcdaa432df
jguerinet <julien@guerinet.com>
2014-01-22 21:08:06 -0500

    Created skeleton for ScheduleActivity

---
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
@@ -0,0 +1,15 @@
+package ca.mcgill.mymcgill.activity;
+
+import android.app.Activity;
+import android.os.Bundle;import ca.mcgill.mymcgill.R;
+
+/**
+ * Author: Julien
+ * Date: 22/01/14, 9:07 PM
+ */
+public class ScheduleActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
