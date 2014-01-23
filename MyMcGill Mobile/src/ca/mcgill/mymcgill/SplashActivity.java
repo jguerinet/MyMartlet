35ce74d36f744fea8837403ba5b6afff23f44cb9
jguerinet <julien@guerinet.com>
2014-01-22 19:36:23 -0500

    Added skeleton for LoginActivity and SplashActivity. Made them portrait-only in the Manifest.

---
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/SplashActivity.java
@@ -0,0 +1,15 @@
+package ca.mcgill.mymcgill;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Author: Julien
+ * Date: 22/01/14, 7:34 PM
+ */
+public class SplashActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
