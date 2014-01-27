2a1d7b63f8f1c7590b171281ff7f92fae5a3c6f0
Shabbir Hussain <mohd.husn001@gmail.com>
2014-01-27 00:09:33 -0500

    activity returns home

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
@@ -1,7 +1,14 @@
 package ca.mcgill.mymcgill.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.os.Bundle;import ca.mcgill.mymcgill.R;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import ca.mcgill.mymcgill.R;
 
 /**
  * Author: Shabbir
@@ -11,8 +18,26 @@ import android.os.Bundle;import ca.mcgill.mymcgill.R;
  */
 public class ScheduleActivity extends Activity {
 	
-    public void onCreate(Bundle savedInstanceState) {
+    @SuppressLint("NewApi")
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        
+     // Make sure we're running on Honeycomb or higher to use ActionBar APIs to return home
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // Show the Up button in the action bar.
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
