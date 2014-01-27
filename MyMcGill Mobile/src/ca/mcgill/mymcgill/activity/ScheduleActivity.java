94775e69802f581765a18ec589d93329a51cce00
Shabbir Hussain <mohd.husn001@gmail.com>
2014-01-26 23:22:18 -0500

    created schedule action

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
@@ -4,10 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;import ca.mcgill.mymcgill.R;
 
 /**
- * Author: Julien
+ * Author: Shabbir
  * Date: 22/01/14, 9:07 PM
+ * 
+ * This Activity loads the schedule from https://horizon.mcgill.ca/pban1/bwskfshd.P_CrseSchd
  */
 public class ScheduleActivity extends Activity {
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
