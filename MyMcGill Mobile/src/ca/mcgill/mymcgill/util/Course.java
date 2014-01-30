f314e313de79b21793df02742b9f0d70659ffc81
Ryan Singzon <rcsingzon@gmail.com>
2014-01-30 18:46:39 -0500

    Added classes for course and transcript information

---
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Course.java
@@ -0,0 +1,10 @@
+package ca.mcgill.mymcgill.util;
+
+/**
+ * Created by Ryan Singzon on 30/01/14.
+ *
+ * This class will contain information pertaining to individual McGill courses that students
+ * have taken, such as the grade, credit, and class average
+ */
+public class Course {
+}
---
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Transcript.java
@@ -0,0 +1,23 @@
+package ca.mcgill.mymcgill.util;
+import java.util.*;
+
+/**
+ * Created by Ryan Singzon on 30/01/14.
+ *
+ * This class will retrieve and parse transcript data from Minerva and hold all the information
+ * that is shown on the transcript
+ *
+ */
+public class Transcript {
+
+    private int cgpa;
+    private int totalCredits;
+    String program;
+
+    ArrayList<Course> semesters = new ArrayList<Course>();
+
+    //Logs into Minerva and returns the user's transcript as a string
+    private String getTranscript(){
+
+    }
+}
