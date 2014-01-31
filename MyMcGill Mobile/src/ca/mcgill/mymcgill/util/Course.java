7830c6e8bc0ccaf342f0f804883ce85d2e40cd2e
Ryan Singzon <rcsingzon@gmail.com>
2014-01-30 20:05:31 -0500

    Updated Transcript to keep a list of semesters, each keeping a list of classes

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Course.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Course.java
@@ -7,4 +7,46 @@ package ca.mcgill.mymcgill.util;
  * have taken, such as the grade, credit, and class average
  */
 public class Course {
+
+    private int credits;
+    private String courseCode;
+    private String courseTitle;
+    private String userGrade;
+    private String averageGrade;
+
+    //Constructor for the Course object
+    public Course(int credits, String courseCode, String courseTitle,
+                    String userGrade, String averageGrade){
+        this.credits = credits;
+        this.courseCode = courseCode;
+        this.courseTitle = courseTitle;
+        this.userGrade = userGrade;
+        this.averageGrade = averageGrade;
+    }
+
+    //Getter for credits
+    public int getCredits(){
+        return credits;
+    }
+
+    //Getter for course code
+    public String getCourseCode(){
+        return courseCode;
+    }
+
+    //Getter for course title
+    public String getCourseTitle(){
+        return courseTitle;
+    }
+
+    //Getter for user grade
+    public String getUserGrade(){
+        return userGrade;
+    }
+
+    //Getter for average grade
+    public String getAverageGrade(){
+        return averageGrade;
+    }
+
 }
--- MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Transcript.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/util/Transcript.java
@@ -12,13 +12,41 @@ public class Transcript {
 
     private int cgpa;
     private int totalCredits;
-    String program;
+    private String transcriptString;
 
-    ArrayList<Course> semesters = new ArrayList<Course>();
+    ArrayList<Semester> semesters = new ArrayList<Semester>();
+
+    //Constructor for the Transcript object
+    public Transcript(){
+
+        //Retrieve transcript data from Minerva as a string
+        transcriptString = getTranscript();
+        parseTranscript(transcriptString);
+    }
 
     //Logs into Minerva and returns the user's transcript as a string
     private String getTranscript(){
         String transcript = "";
         return transcript;
+    }
+
+    //Create an array of semesters and set CGPA and total credits
+    private void parseTranscript(String transcriptString){
+        //TODO Parse transcript and obtain array of semesters and CGPA and total credits
+    }
+
+    //Getter for CGPA
+    public int getCgpa(){
+        return cgpa;
+    }
+
+    //Getter for totalCredits
+    public int getTotalCredits(){
+        return totalCredits;
+    }
+
+    //Getter for semesters
+    public ArrayList<Semester> getSemesters(){
+        return semesters;
     }
 }
