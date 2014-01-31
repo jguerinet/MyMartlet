721c21ad4ffe6723b6c3236cf060c84f5aa13632
JDAlfaro <JoshuaDavidAlfaro@gmail.com>
2014-01-30 21:23:33 -0500

    tried to display html of schedule in schedule acitivty

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
@@ -1,8 +1,17 @@
 package ca.mcgill.mymcgill.activity;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Bundle;import ca.mcgill.mymcgill.R;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.text.Html;
+import android.view.MenuItem;
+import android.widget.TextView;
+import ca.mcgill.mymcgill.R;
 
+import ca.mcgill.mymcgill.util.Connection;
+import ca.mcgill.mymcgill.util.Constants;
 /**
  * Author: Julien
  * Date: 22/01/14, 9:07 PM
@@ -11,5 +20,42 @@ public class ScheduleActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-    }
+        
+        TextView textView = new TextView(this);
+        String htmlAsAString = Connection.getUrl("");
+        textView.setText(Html.fromHtml(htmlAsAString));
+        //textView.setText("hell0");
+        setContentView(textView);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 }
