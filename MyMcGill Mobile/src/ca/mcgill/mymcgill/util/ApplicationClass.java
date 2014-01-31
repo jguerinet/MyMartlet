dcedf56ca74c8db00c51f1fb11cee32e673c1c8e
jguerinet <julien@guerinet.com>
2014-01-31 18:28:51 -0500

    Set up the icon font (FontAwesome)

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/util/ApplicationClass.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/util/ApplicationClass.java
@@ -1,6 +1,8 @@
 package ca.mcgill.mymcgill.util;
 
 import android.app.Application;
+import android.content.Context;
+import android.graphics.Typeface;
 
 import ca.mcgill.mymcgill.object.Transcript;
 
@@ -11,17 +13,32 @@ import ca.mcgill.mymcgill.object.Transcript;
  * Will contain relevant objects that were loaded from the storage, and will be updated upon sign-in.
  */
 public class ApplicationClass extends Application {
+    private static Context context;
+
+    private static Typeface iconFont;
+
     private static Transcript transcript;
 
     @Override
     public void onCreate(){
         super.onCreate();
 
+        //Set the static context (used in loading the font)
+        context = this;
+
         //Load the transcript
         transcript = Load.loadTranscript(this);
     }
 
     /* GETTER METHODS */
+    private static Typeface getIconFont(){
+        if(iconFont == null){
+            iconFont = Typeface.createFromAsset(context.getAssets(), "icon-font.ttf");
+        }
+
+        return iconFont;
+    }
+
     public static Transcript getTranscript(){
         return transcript;
     }
