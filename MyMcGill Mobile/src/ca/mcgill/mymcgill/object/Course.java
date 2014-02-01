9458addce8cf5320d80606e6e7fcde4e7fb15d6e
jguerinet <julien@guerinet.com>
2014-02-01 11:05:25 -0500

    Made Course Serializable.

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/object/Course.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/object/Course.java
@@ -1,12 +1,14 @@
 package ca.mcgill.mymcgill.object;
 
+import java.io.Serializable;
+
 /**
  * Created by Ryan Singzon on 30/01/14.
  *
  * This class will contain information pertaining to individual McGill courses that students
  * have taken, such as the grade, credit, and class average
  */
-public class Course {
+public class Course implements Serializable{
 
     private int credits;
     private String courseCode;
