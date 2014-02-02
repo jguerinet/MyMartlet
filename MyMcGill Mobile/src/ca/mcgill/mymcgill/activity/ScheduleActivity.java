f3880fb2c4ac1ef79c947711167476d07faad736
Nhat-Quang Dao <nhat.quang.d@gmail.com>
2014-02-02 05:15:50 -0500

    parser for schedule and modified coursesched to accept int for day

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ScheduleActivity.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -19,6 +21,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ca.mcgill.mymcgill.objects.CourseSched;
 /**
  * Author: Shabbir
  * Date: 22/01/14, 9:07 PM
@@ -26,7 +29,7 @@ import org.jsoup.select.Elements;
  * This Activity loads the schedule from https://horizon.mcgill.ca/pban1/bwskfshd.P_CrseSchd
  */
 public class ScheduleActivity extends Activity {
-	
+	ArrayList<CourseSched> courseList = new ArrayList<CourseSched>();
     @SuppressLint("NewApi")
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,8 +45,11 @@ public class ScheduleActivity extends Activity {
         String fileContent = readFromFile("minsched.html");
         
         Document doc = Jsoup.parse(fileContent);
-        Elements forms = doc.getElementsByTag("table");
-        
+        Element scheduleTable = doc.getElementsByClass("datadisplaytable").first();
+        Elements weekCell = doc.getElementsByClass("fieldlargetext");
+        Elements scheduleRows = scheduleTable.getElementsByTag("tr");
+        getWeek(weekCell.first());
+        getSchedule(scheduleRows);
         
         
         
@@ -51,11 +57,44 @@ public class ScheduleActivity extends Activity {
         
     }
     
-    private void getWeek(Element form){
-    	
+    private String getWeek(Element form){
+    	return form.text();
     }
-    private void getSchedule(Element form){
-    	
+    
+    /**
+     * This method takes the list of rows in the table and populate the courseList
+     * @param rows
+     */
+    private void getSchedule(Elements rows){
+    	for (int i = 0; i < rows.size(); i++) {
+        	Element row = rows.get(i);
+        	Elements cells = row.getElementsByTag("td");
+        	if (cells.size() < 7) {
+        		continue;
+        	}
+        	for (int day = 0; day < cells.size(); day++) {
+        		Element cell = cells.get(day);
+        		Elements courseCells = cell.getElementsByClass("ddlabel");
+        		if (courseCells.size() < 1) {
+            		continue;
+            	}
+        		Elements cellContent = courseCells.first().getElementsByTag("a");
+        		Element courseCell = cellContent.first();
+            	String courseHtml = courseCell.html();
+            	String courseString = courseHtml.replaceAll("<br />", ",");
+            	String[] attributes = courseString.split(",");
+            	String courseCode = attributes[0];
+            	String room = attributes[3];
+            	int crn = Integer.parseInt(attributes[1].split(" ")[0]);
+            	String[] times = attributes[2].split("-");
+            	int startHour = Integer.parseInt(times[0].split(" ")[0].split(":")[0]);
+            	int startMinute = Integer.parseInt(times[0].split(" ")[0].split(":")[1]);
+            	int endHour = Integer.parseInt(times[1].split(" ")[0].split(":")[0]);
+            	int endMinute = Integer.parseInt(times[1].split(" ")[0].split(":")[1]);
+            	CourseSched schedule = new CourseSched(crn, courseCode, day, startHour, startMinute, endHour, endMinute, room);
+            	courseList.add(schedule);
+        	}
+        }
     }
 
     @Override
