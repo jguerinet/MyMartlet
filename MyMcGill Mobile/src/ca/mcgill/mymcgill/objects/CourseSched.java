f9edbe07be7e86c87107763ce958d502953d7ef3
jguerinet <julien@guerinet.com>
2014-02-03 16:16:58 -0500

    Moved the getDay() by character to the Day Enum

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/objects/CourseSched.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/objects/CourseSched.java
@@ -21,29 +21,7 @@ public class CourseSched implements Serializable{
 	public CourseSched(int crn, String courseCode, char day, int startHour, int startMinute, int endHour, int endMinute, String room) {
 		this.crn = crn;
 		this.courseCode = courseCode;
-        switch (day) {
-        	case 'M': 
-        		this.day = Day.MONDAY;
-        		break;
-        	case 'T': 
-        		this.day = Day.TUESDAY;
-        		break;
-        	case 'W': 
-        		this.day = Day.WEDNESDAY;
-        		break;
-        	case 'R': 
-        		this.day = Day.THURSDAY;
-        		break;
-        	case 'F': 
-        		this.day = Day.FRIDAY;
-        		break;
-        	case 'S': 
-        		this.day = Day.SATURDAY;
-        		break;
-        	case 'N': 
-        		this.day = Day.SUNDAY;
-        		break;
-        }
+        this.day = Day.getDay(day);
 		this.startH = startHour;
 		this.startM = startMinute;
 		this.endH = endHour;
