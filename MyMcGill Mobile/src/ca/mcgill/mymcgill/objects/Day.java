81e86aee8f93492b3ff26aad7b4a134556873120
jguerinet <julien@guerinet.com>
2014-02-02 22:35:24 -0500

    Added helper methods in Day class to get day based on integer, and get string of a day.

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/objects/Day.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/objects/Day.java
@@ -1,5 +1,9 @@
 package ca.mcgill.mymcgill.objects;
 
+import android.content.Context;
+
+import ca.mcgill.mymcgill.R;
+
 /**
  * Author: Julien
  * Date: 01/02/14, 6:54 PM
@@ -11,5 +15,49 @@ public enum Day {
     THURSDAY,
     FRIDAY,
     SATURDAY,
-    SUNDAY
+    SUNDAY;
+
+    //Get the day based on the number from 1-7
+    public static Day getDay(int dayNumber){
+        switch (dayNumber) {
+            case 0:
+                return Day.MONDAY;
+            case 1:
+                return Day.TUESDAY;
+            case 2:
+                return Day.WEDNESDAY;
+            case 3:
+                return Day.THURSDAY;
+            case 4:
+                return Day.FRIDAY;
+            case 5:
+                return Day.SATURDAY;
+            case 6:
+                return Day.SUNDAY;
+            default:
+                return null;
+        }
+    }
+
+    //Get the string for a given day
+    public String getDayString(Context context){
+        switch(this){
+            case MONDAY:
+                return context.getResources().getString(R.string.monday);
+            case TUESDAY:
+                return context.getResources().getString(R.string.tuesday);
+            case WEDNESDAY:
+                return context.getResources().getString(R.string.wednesday);
+            case THURSDAY:
+                return context.getResources().getString(R.string.thursday);
+            case FRIDAY:
+                return context.getResources().getString(R.string.friday);
+            case SATURDAY:
+                return context.getResources().getString(R.string.saturday);
+            case SUNDAY:
+                return context.getResources().getString(R.string.sunday);
+            default:
+                return null;
+        }
+    }
 }
