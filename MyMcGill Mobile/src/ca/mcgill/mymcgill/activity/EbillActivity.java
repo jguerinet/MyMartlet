8208e1f7af7647ea0ef5e0615342a3d213ff16ee
Adnan Uddin <adnan.uddin6@gmail.com>
2014-02-04 00:22:39 -0500

    added ebill activity and ebill item

---
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/EbillActivity.java
@@ -0,0 +1,25 @@
+package ca.mcgill.mymcgill.activity;
+
+import ca.mcgill.mymcgill.R;
+import ca.mcgill.mymcgill.R.layout;
+import ca.mcgill.mymcgill.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class EbillActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ebill);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.ebill, menu);
+		return true;
+	}
+
+}
