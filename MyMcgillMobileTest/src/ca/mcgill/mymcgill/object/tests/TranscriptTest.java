8dae887e5b011109c6c49e13ac4c06b5bd72ca50
JDAlfaro <JoshuaDavidAlfaro@gmail.com>
2014-02-07 13:50:42 -0500

    working on testing Transcript

--- MyMcgillMobileTest/src/ca/mcgill/mymcgill/object/tests/TranscriptTest.java
+++ MyMcgillMobileTest/src/ca/mcgill/mymcgill/object/tests/TranscriptTest.java
@@ -1,14 +1,22 @@
 package ca.mcgill.mymcgill.object.tests;
 
 import static org.junit.Assert.*;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
+
+import ca.mcgill.mymcgill.object.Transcript;
 
 public class TranscriptTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetCGPA() throws IOException {
+		FileReader reader = new FileReader("Desktop/testTranscript.html");
+		String text = reader.toString();
+		Transcript testTranscript = new Transcript(text);
+		double gpa = testTranscript.getCgpa();
+		assertEquals("CGPA is incorrect", 3, gpa, 0.01);
+		reader.close();
 	}
-
 }
