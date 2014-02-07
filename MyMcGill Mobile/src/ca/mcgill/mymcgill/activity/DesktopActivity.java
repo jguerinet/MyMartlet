2c683accbacc7ab9e52d638997ee5964010e748b
Shabbir Hussain <mohd.husn001@gmail.com>
2014-02-06 22:40:24 -0500

    added javascript fix for desktopsite

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/DesktopActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/DesktopActivity.java
@@ -26,8 +26,8 @@ public class DesktopActivity extends Activity {
         webView.loadUrl("https://mymcgill.mcgill.ca/portal/page/portal/Login");
         webView.setWebViewClient(new WebViewClient() {
             public void onPageFinished(WebView view, String url) {
-                view.loadUrl("javascript:(function(){document.getElementById('username').value='" +
-                        Load.loadUsername(DesktopActivity.this) + "';" +
+                view.loadUrl("javascript:(window.onload = function(){document.getElementById('username').value='" +
+                        Load.loadUsername(DesktopActivity.this) + "@mail.mcgill.ca';" +
                         "document.getElementById('password').value='" +
                         Load.loadPassword(DesktopActivity.this) + "'; document.LoginForm.submit(); })()");
             }
