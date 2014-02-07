2f23a94c5690cf84bd0900b9fd31c005cf06f319
JDAlfaro <JoshuaDavidAlfaro@gmail.com>
2014-02-07 18:36:23 -0500

    failing to read file

--- MyMcgillMobileTest/src/ca/mcgill/mymcgill/object/tests/TranscriptTest.java
+++ MyMcgillMobileTest/src/ca/mcgill/mymcgill/object/tests/TranscriptTest.java
@@ -1,14 +1,18 @@
 package ca.mcgill.mymcgill.object.tests;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 import android.os.Bundle;
+import android.test.AndroidTestCase;
 
 import ca.mcgill.mymcgill.R;
 import ca.mcgill.mymcgill.activity.transcript.TranscriptActivity;
@@ -16,17 +20,20 @@ import ca.mcgill.mymcgill.activity.transcript.tests.TranscriptActivityTest;
 import ca.mcgill.mymcgill.object.Transcript;
 import ca.mcgill.mymcgill.util.Help;
 
-public class TranscriptTest extends TestCase{
+public class TranscriptTest extends TestCase {
 
 	@Test
-	public void testGetCGPA() {
-		TranscriptActivity testAct = new TranscriptActivity();
-		testAct.onCreate(new Bundle());
-		
-		String text = Help.readFromFile(testAct, R.raw.test_transcript);
+	public void testGetCGPA() throws IOException {
+		Scanner scan = new Scanner(new File("./test_transcript.txt"));
+		String text = "";
+		while(scan.hasNext())
+		{
+			text = text + scan.next();
+		}
 		Transcript testTranscript = new Transcript(text);
 		double gpa = testTranscript.getCgpa();
 		assertEquals("CGPA is incorrect", 3.92, gpa, 0.01);
+		scan.close();
 	}
 	
 	@Test
