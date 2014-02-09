f261c5baeac43dd81ba9d61880db8754b68da307
Shabbir Hussain <mohd.husn001@gmail.com>
2014-02-09 15:33:19 -0500

    text cgpa fixed

--- mymcgilltest-test/src/ca/mcgill/mymcgill/object/TranscriptTest.java
+++ mymcgilltest-test/src/ca/mcgill/mymcgill/object/TranscriptTest.java
@@ -1,29 +1,36 @@
 package ca.mcgill.mymcgill.object;
 
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import android.os.Bundle;
-import android.test.AndroidTestCase;
-
 import ca.mcgill.mymcgill.R;
 import ca.mcgill.mymcgill.activity.transcript.TranscriptActivity;
-
-import ca.mcgill.mymcgill.object.Transcript;
 import ca.mcgill.mymcgill.util.Help;
 
 public class TranscriptTest extends TestCase {
 
 	@Test
 	public void testGetCGPA() throws IOException {
-		Scanner scan = new Scanner(new File("C:\\Users\\c\\workspace\\TestScanner\\src\\testBill.txt"));
-		String text = "";
+		
+		String file = "res/raw/test_transcript.txt";
+		InputStream in = this.getClass().getClassLoader().getResourceAsStream(file);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		
+		String text ="";
+		StringBuilder sb = new StringBuilder();
+		
+		while((text = reader.readLine()) != null){
+			sb.append(text);
+		}
+		
+		text = sb.toString();
+		
 		//while(scan.hasNext())
 		//{
 		//	text = text + scan.next();
@@ -31,7 +38,6 @@ public class TranscriptTest extends TestCase {
 		Transcript testTranscript = new Transcript(text);
 		double gpa = testTranscript.getCgpa();
 		assertEquals("CGPA is incorrect", 3.92, gpa, 0.01);
-		scan.close();
 	}
 	
 	@Test
