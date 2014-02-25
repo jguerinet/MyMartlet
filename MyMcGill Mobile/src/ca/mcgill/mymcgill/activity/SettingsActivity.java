5f84525ab0d8c39c569199e29c81c0c7a327303d
Shabbir <mohd.husn001@gmail.com>
2014-02-25 02:35:38 -0500

    Added Language Settings

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/SettingsActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/SettingsActivity.java
@@ -1,6 +1,8 @@
 package ca.mcgill.mymcgill.activity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import ca.mcgill.mymcgill.R;
 import ca.mcgill.mymcgill.activity.drawer.DrawerActivity;
@@ -13,5 +15,14 @@ public class SettingsActivity extends DrawerActivity {
         mDrawerAdapter = new DrawerAdapter(this, DrawerAdapter.SETTINGS_POSITION);
         super.onCreate(savedInstanceState);
 
-	 } 
+        //Set up the info
+        Spinner languages = (Spinner)findViewById(R.id.settings_language);
+        //Standard ArrayAdapter
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.settings_languages, android.R.layout.simple_spinner_item);
+        //Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply the adapter to the spinner
+        languages.setAdapter(adapter);
+    }
 }
