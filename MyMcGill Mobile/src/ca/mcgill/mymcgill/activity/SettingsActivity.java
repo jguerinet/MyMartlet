c6af9030da9d347b7d1ffaa44d9113dd6ed7bc6a
Shabbir <mohd.husn001@gmail.com>
2014-02-25 02:39:06 -0500

    Added Homepage Settings

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/SettingsActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/SettingsActivity.java
@@ -18,11 +18,20 @@ public class SettingsActivity extends DrawerActivity {
         //Set up the info
         Spinner languages = (Spinner)findViewById(R.id.settings_language);
         //Standard ArrayAdapter
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        ArrayAdapter<CharSequence> languageAdapter = ArrayAdapter.createFromResource(this,
                 R.array.settings_languages, android.R.layout.simple_spinner_item);
         //Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Apply the adapter to the spinner
-        languages.setAdapter(adapter);
+        languages.setAdapter(languageAdapter);
+
+        Spinner homepages = (Spinner)findViewById(R.id.settings_homepage);
+        //Standard ArrayAdapter
+        ArrayAdapter<CharSequence> homepageAdapter = ArrayAdapter.createFromResource(this,
+                R.array.settings_homepages, android.R.layout.simple_spinner_item);
+        //Specify the layout to use when the list of choices appears
+        homepageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Apply the adapter to the spinner
+        homepages.setAdapter(homepageAdapter);
     }
 }
