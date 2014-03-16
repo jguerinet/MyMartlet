5eb135d2bfc40b28dc55d2e9ef13665559aad56c
JDAlfaro <JoshuaDavidAlfaro@gmail.com>
2014-03-16 18:25:26 -0400

    improved change semester dialog

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ChangeSemesterActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/ChangeSemesterActivity.java
@@ -1,10 +1,15 @@
 package ca.mcgill.mymcgill.activity;
 
 import android.annotation.SuppressLint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import ca.mcgill.mymcgill.R;
@@ -23,39 +28,39 @@ public class ChangeSemesterActivity extends BaseActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        //overridePendingTransition(R.anim.in_from_top, R.anim.stay);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        overridePendingTransition(R.anim.in_from_top, R.anim.stay);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_change_semester);
     	
-//        //Get the screen height
-//        Display display = getWindowManager().getDefaultDisplay();
-//        Point size = new Point();
-//
-//        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB_MR2) {
-//            size.set(display.getWidth(), display.getHeight());
-//        }
-//        else{
-//            display.getSize(size);
-//        }
-//
-//        int displayWidth = size.x;
-//        int displayHeight = size.y;
-//
-//        //Set the width and height to 2/3 of the screen
-//        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_course_container);
-//
-//        ViewGroup.LayoutParams params = layout.getLayoutParams();
-//        //Quick check
-//        assert (params != null);
-//        params.height = (2 * displayHeight) / 3;
-//        params.width = (5 * displayWidth) / 6;
-//        layout.setLayoutParams(params);
+        //Get the screen height
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+
+        if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB_MR2) {
+            size.set(display.getWidth(), display.getHeight());
+        }
+        else{
+            display.getSize(size);
+        }
+
+        int displayWidth = size.x;
+        int displayHeight = size.y;
+
+        //Set the width and height to 2/3 of the screen
+        LinearLayout layout = (LinearLayout) findViewById(R.id.activity_change_semester);
+
+        ViewGroup.LayoutParams params = layout.getLayoutParams();
+        //Quick check
+        assert (params != null);
+        params.height = (2 * displayHeight) / 3;
+        params.width = (5 * displayWidth) / 6;
+        layout.setLayoutParams(params);
 
         Spinner season = (Spinner) findViewById(R.id.change_semester_season);
     	
     	//Standard ArrayAdapter
         ArrayAdapter<CharSequence> seasonAdapter = ArrayAdapter.createFromResource(this,
-                R.array.season_entries, android.R.layout.simple_spinner_item);
+                R.array.change_semester_season, android.R.layout.simple_spinner_item);
         //Specify the layout to use when the list of choices appears
         seasonAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Apply the adapter to the spinner
@@ -100,8 +105,13 @@ public class ChangeSemesterActivity extends BaseActivity {
 //        });
     }
 
-//    public void done(View v){
-//        finish();
-//        overridePendingTransition(R.anim.stay, R.anim.out_to_top);
-//    }
+    public void cancelPress(View v){
+        finish();
+        overridePendingTransition(R.anim.stay, R.anim.out_to_top);
+    }
+    
+    public void okPress(View v){
+        finish();
+        overridePendingTransition(R.anim.stay, R.anim.out_to_top);
+    }
 }
