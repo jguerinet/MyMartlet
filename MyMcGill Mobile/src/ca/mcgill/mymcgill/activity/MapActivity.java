65bce3ca95c6f3e329874c9d59cf00b0fe0a52b6
Ryan Singzon <rcsingzon@gmail.com>
2014-03-18 11:27:38 -0400

    Working on displaying map, getting error inflating class fragment in activity_map

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/MapActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/MapActivity.java
@@ -2,16 +2,19 @@ package ca.mcgill.mymcgill.activity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Window;
+import android.widget.Toast;
 
 
 import ca.mcgill.mymcgill.R;
 import ca.mcgill.mymcgill.activity.drawer.DrawerActivity;
 
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.MapFragment;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Ryan Singzon on 14/03/14.
@@ -19,7 +22,7 @@ import ca.mcgill.mymcgill.activity.drawer.DrawerActivity;
 public class MapActivity extends DrawerActivity {
 
 
-//    private GoogleMap mMap;
+    private GoogleMap mMap;
 
     @SuppressLint("NewApi")
      @Override
@@ -28,6 +31,17 @@ public class MapActivity extends DrawerActivity {
         setContentView(R.layout.activity_map);
         super.onCreate(savedInstanceState);
 
+        /*if (mMap == null) {
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+
+            // check if map is created successfully or not
+            if (mMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }*/
     }
 
 
