795195c1a6833709eccd6448196156330fbf5033
Julien <julien@guerinet.com>
2014-03-22 17:47:42 -0400

    Added Season enum and helper methods

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/object/ScheduleSemester.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/object/ScheduleSemester.java
@@ -1,25 +1,37 @@
 package ca.mcgill.mymcgill.object;
 
+import android.content.Context;
+
 import java.io.Serializable;
+
+import ca.mcgill.mymcgill.util.Connection;
 
 /**
  * Author: Julien
  * Date: 2014-03-22 17:30
  */
 public class ScheduleSemester implements Serializable{
-    private String mSeason;
+    private Season mSeason;
     private int mYear;
 
-    public ScheduleSemester(String season, int year){
+    public ScheduleSemester(Season season, int year){
         this.mSeason = season;
         this.mYear = year;
     }
 
-    public String getSeason(){
+    public Season getSeason(){
         return this.mSeason;
     }
 
     public int getYear(){
         return this.mYear;
+    }
+
+    public String getURL(){
+        return Connection.minervaSchedulePrefix + mYear + mSeason.getSeasonNumber();
+    }
+
+    public String toString(Context context){
+        return mSeason.toString(context) + " " + mYear;
     }
 }
