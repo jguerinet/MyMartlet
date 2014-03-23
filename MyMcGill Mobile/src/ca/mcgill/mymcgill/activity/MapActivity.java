04ed1cc3ae3b8f7abfb69fa8929f3c778ed84a24
Ryan Singzon <rcsingzon@gmail.com>
2014-03-23 16:51:23 -0400

    Map centers on McGill campus

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/MapActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/MapActivity.java
@@ -4,7 +4,13 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Window;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import ca.mcgill.mymcgill.R;
 import ca.mcgill.mymcgill.activity.drawer.DrawerFragmentActivity;
@@ -25,17 +31,17 @@ public class MapActivity extends DrawerFragmentActivity {
         setContentView(R.layout.activity_map);
         showDrawer(true);
 
-//        if (mMap == null) {
-//            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(
-//                    R.id.map)).getMap();
-//
-//            // check if map is created successfully or not
-//            if (mMap == null) {
-//                Toast.makeText(getApplicationContext(),
-//                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-//                        .show();
-//            }
-//        }
+        final LatLng MCGILL = new LatLng(45.504723,-73.576975);
+
+        mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (mMap!=null){
+            Marker mcgill = mMap.addMarker(new MarkerOptions().position(MCGILL).title("McGill"));
+            mcgill.setVisible(false);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MCGILL, 16));
+            mMap.setMyLocationEnabled(true);
+        }
+
+
     }
 
 
