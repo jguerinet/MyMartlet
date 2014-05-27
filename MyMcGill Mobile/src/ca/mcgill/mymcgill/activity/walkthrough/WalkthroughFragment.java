9943b199b2fa302026f7c31952ab874994bfbf65
Julien Guerinet <julien@guerinet.com>
2014-05-26 21:23:56 -0400

    Created the layouts for all of the walkthrough fragments.

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/walkthrough/WalkthroughFragment.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/walkthrough/WalkthroughFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import ca.mcgill.mymcgill.R;
+
 /**
  * Author : Yulric
  * Date :  17/11/13 10:28 AM
@@ -34,44 +36,45 @@ public class WalkthroughFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View pageView = null;
+        View pageView;
 
         switch(mPageNumber){
             //Welcome to the MyMcGill App
             case 0:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_0, container);
                 break;
             //Access all of your MyMcGill essentials easily
-            case 1:{
+            case 1:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_1, container);
                 break;
-            }
 
             //Main Menu Explanation
-            case 2:{
+            case 2:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_2, container);
                break;
-            }
 
             //Offline Access
-            case 3:{
+            case 3:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_3, container);
                 break;
-            }
 
             //Security
-            case 4:{
+            case 4:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_4, container);
                 break;
-            }
 
             //Help/About Pages
-            case 5:{
+            case 5:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_5, container);
                break;
-            }
 
             //Default Homepage
-            case 6:{
+            case 6:
+                pageView = View.inflate(getActivity(), R.layout.fragment_walkthrough_6, container);
                break;
-            }
-            default:{
+
+            default:
                 return null;
-            }
         }
 
         return pageView;
