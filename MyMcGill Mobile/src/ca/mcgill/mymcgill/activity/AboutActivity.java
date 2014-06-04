603b78937ebb5aa78f122a233ab2f5680f02e6cf
Julien Guerinet <julien@guerinet.com>
2014-06-03 21:49:57 -0400

    Changed tab text color to black.

--- MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/AboutActivity.java
+++ MyMcGill Mobile/src/ca/mcgill/mymcgill/activity/AboutActivity.java
@@ -2,14 +2,11 @@ package ca.mcgill.mymcgill.activity;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.widget.TextView;
 
 import ca.mcgill.mymcgill.R;
 import ca.mcgill.mymcgill.activity.drawer.DrawerFragmentActivity;
@@ -28,6 +25,7 @@ public class AboutActivity extends DrawerFragmentActivity {
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+        //Set up the ViewPager
         mPager = (ViewPager)findViewById(R.id.about_viewpager);
         AboutPagerAdapter adapter = new AboutPagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(adapter);
@@ -42,7 +40,7 @@ public class AboutActivity extends DrawerFragmentActivity {
             public void onPageScrollStateChanged(int i) {}
         });
 
-        //Tab Listener
+        //Set up the Tab Listener
         ActionBar.TabListener tabListener = new ActionBar.TabListener() {
             public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
                 mPager.setCurrentItem(tab.getPosition());
@@ -51,25 +49,13 @@ public class AboutActivity extends DrawerFragmentActivity {
             public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {}
         };
 
-        // add tabs
-
-        TextView teamName = new TextView(this);
-        teamName.setText(getResources().getString(R.string.about_team_name));
-        teamName.setTextColor(Color.BLACK);
-        teamName.setPadding(0,20,0,0); // need to make padding relative
-
-        TextView help = new TextView(this);
-        help.setText(getResources().getString(R.string.about_help));
-        help.setTextColor(Color.BLACK);
-        help.setGravity(Gravity.CENTER);
-        help.setPadding(0,20,0,0); // need to make padding relative
-
-
-        ActionBar.Tab teamNameTab = actionBar.newTab().setCustomView(teamName).setTabListener(tabListener);
-        ActionBar.Tab helpTab = actionBar.newTab().setCustomView(help).setTabListener(tabListener);
-
-        actionBar.addTab(teamNameTab);
-        actionBar.addTab(helpTab);
+        //Add the tabs
+        actionBar.addTab(actionBar.newTab()
+                .setText("About")
+                .setTabListener(tabListener));
+        actionBar.addTab(actionBar.newTab()
+                .setText("Help")
+                .setTabListener(tabListener));
     }
 
     class AboutPagerAdapter extends FragmentStatePagerAdapter{
