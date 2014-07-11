0107c566cf12174e53f4ec1dea69f34e89d60fb8
Julien Guerinet <julien@guerinet.com>
2014-07-11 20:45:05 +0200

    Renamed MyMcGill Mobile module to McGill Mobile

--- McGill Mobile/src/ca/appvelopers/mcgillmobile/util/Connection.java
+++ McGill Mobile/src/ca/appvelopers/mcgillmobile/util/Connection.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.util.Log;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
