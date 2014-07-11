e85a9628a40d59ad5d7a748c32365e7526701485
Julien Guerinet <julien@guerinet.com>
2014-07-11 23:25:25 +0200

    Made the Place and PlaceCategory object and enum

---
+++ McGill Mobile/src/ca/appvelopers/mcgillmobile/object/Place.java
@@ -0,0 +1,12 @@
+package ca.appvelopers.mcgillmobile.object;
+
+/**
+ * Author: Julien Guerinet
+ * Date: 2014-07-11 11:24 PM
+ * Copyright (c) 2014 Julien Guerinet. All rights reserved.
+ * Represents a place on the map
+ */
+
+public class Place {
+
+}
---
+++ McGill Mobile/src/ca/appvelopers/mcgillmobile/object/PlaceCategory.java
@@ -0,0 +1,11 @@
+package ca.appvelopers.mcgillmobile.object;
+
+/**
+ * Author: Julien Guerinet
+ * Date: 2014-07-11 11:24 PM
+ * Copyright (c) 2014 Julien Guerinet. All rights reserved.
+ * Represents the category of a place
+ */
+
+public enum PlaceCategory {
+}
