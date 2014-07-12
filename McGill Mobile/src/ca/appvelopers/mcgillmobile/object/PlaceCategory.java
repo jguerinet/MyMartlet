e99b0ec55584b5b421b290a2b579f7d24a5b8a62
Julien Guerinet <julien@guerinet.com>
2014-07-12 21:48:39 +0200

    Added Strings for the different Place categories

--- McGill Mobile/src/ca/appvelopers/mcgillmobile/object/PlaceCategory.java
+++ McGill Mobile/src/ca/appvelopers/mcgillmobile/object/PlaceCategory.java
@@ -1,5 +1,9 @@
 package ca.appvelopers.mcgillmobile.object;
 
+import android.content.Context;
+
+import ca.appvelopers.mcgillmobile.R;
+
 /**
  * Author: Julien Guerinet
  * Date: 2014-07-11 11:24 PM
@@ -8,15 +12,44 @@ package ca.appvelopers.mcgillmobile.object;
  */
 
 public enum PlaceCategory {
-    CLASS,
+    BUILDING,
     FOOD,
     SOCIAL,
     ADMINISTRATION,
     ATHLETICS,
-    LIBRARIES,
-    MUSEUMS,
-    RESIDENCES,
+    LIBRARY,
+    MUSEUM,
+    RESIDENCE,
     SECURITY,
-    STUDENT_ASSOCIATIONS,
-    FACULTY_OFFICES
+    STUDENT_ASSOCIATION,
+    FACULTY_OFFICE;
+
+    public String toString(Context context){
+        switch(this){
+            case BUILDING:
+                return context.getString(R.string.map_building);
+            case FOOD:
+                return context.getString(R.string.map_food);
+            case SOCIAL:
+                return context.getString(R.string.map_social);
+            case ADMINISTRATION:
+                return context.getString(R.string.map_administration);
+            case ATHLETICS:
+                return context.getString(R.string.map_athletics);
+            case LIBRARY:
+                return context.getString(R.string.map_library);
+            case MUSEUM:
+                return context.getString(R.string.map_museum);
+            case RESIDENCE:
+                return context.getString(R.string.map_residence);
+            case SECURITY:
+                return context.getString(R.string.map_security);
+            case STUDENT_ASSOCIATION:
+                return context.getString(R.string.map_student_associations);
+            case FACULTY_OFFICE:
+                return context.getString(R.string.map_faculty_offices);
+            default:
+                return null;
+        }
+    }
 }
