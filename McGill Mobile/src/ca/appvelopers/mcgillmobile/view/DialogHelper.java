947c6d857050663bff24354e38ef2de045a9ad6e
Julien Guerinet <julien@guerinet.com>
2014-09-22 19:05:17 -0400

    Added the Do Not Show again for the parse error

--- McGill Mobile/src/ca/appvelopers/mcgillmobile/view/DialogHelper.java
+++ McGill Mobile/src/ca/appvelopers/mcgillmobile/view/DialogHelper.java
@@ -1,13 +1,16 @@
 package ca.appvelopers.mcgillmobile.view;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.View;
+import android.widget.CheckBox;
 
 import ca.appvelopers.mcgillmobile.App;
 import ca.appvelopers.mcgillmobile.R;
 import ca.appvelopers.mcgillmobile.util.Help;
+import ca.appvelopers.mcgillmobile.util.Load;
+import ca.appvelopers.mcgillmobile.util.Save;
 
 /**
  * Author: Julien
@@ -38,22 +41,35 @@ public class DialogHelper {
     public static void showTranscriptBugDialog(final String className, final String exception) {
         final Context context = App.getContext();
 
-        new AlertDialog.Builder(context)
-                .setMessage(context.getString(R.string.bug_parser_transcript))
-                .setPositiveButton(context.getString(R.string.bug_parser_yes), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Help.sendBugReport(context, context.getString(R.string.bug_parser_transcript_title, className), exception);
-                    }
-                })
-                .setNegativeButton(context.getString(R.string.bug_parser_no), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .create()
-                .show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        View checkboxLayout = View.inflate(context, R.layout.dialog_checkbox, null);
+        final CheckBox dontShowAgain = (CheckBox) checkboxLayout.findViewById(R.id.skip);
+        builder.setView(checkboxLayout);
+        builder.setTitle(context.getString(R.string.warning));
+        builder.setMessage(context.getString(R.string.bug_parser_transcript));
+        builder.setPositiveButton(context.getString(R.string.bug_parser_yes), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Send the bug report
+                Help.sendBugReport(context, context.getString(R.string.bug_parser_transcript_title, className), exception);
+
+                //Save the do not show option
+                Save.saveParserErrorDoNotShow(context, dontShowAgain.isChecked());
+            }
+        });
+        builder.setNegativeButton(context.getString(R.string.bug_parser_no), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss the dialog
+                dialog.dismiss();
+
+                //Save the do not show again
+                Save.saveParserErrorDoNotShow(context, dontShowAgain.isChecked());
+            }
+        });
+
+        //Only show if they have not checked "Do not show again" already
+        if(!Load.loadParserErrorDoNotShow(context)){
+            builder.show();
+        }
     }
 
     /**
@@ -65,21 +81,34 @@ public class DialogHelper {
     public static void showSemesterBugDialog(final String term, final String className, final String exception) {
         final Context context = App.getContext();
 
-        new AlertDialog.Builder(context)
-                .setMessage(context.getString(R.string.bug_parser_semester, term))
-                .setPositiveButton(context.getString(R.string.bug_parser_yes), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Help.sendBugReport(context, context.getString(R.string.bug_parser_semester_title, term, className), exception);
-                    }
-                })
-                .setNegativeButton(context.getString(R.string.bug_parser_no), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .create()
-                .show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        View checkboxLayout = View.inflate(context, R.layout.dialog_checkbox, null);
+        final CheckBox dontShowAgain = (CheckBox) checkboxLayout.findViewById(R.id.skip);
+        builder.setView(checkboxLayout);
+        builder.setTitle(context.getString(R.string.warning));
+        builder.setMessage(context.getString(R.string.bug_parser_semester, term));
+        builder.setPositiveButton(context.getString(R.string.bug_parser_yes), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Send the bug report
+                Help.sendBugReport(context, context.getString(R.string.bug_parser_semester_title, term, className), exception);
+
+                //Save the do not show option
+                Save.saveParserErrorDoNotShow(context, dontShowAgain.isChecked());
+            }
+        });
+        builder.setNegativeButton(context.getString(R.string.bug_parser_no), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss the dialog
+                dialog.dismiss();
+
+                //Save the do not show again
+                Save.saveParserErrorDoNotShow(context, dontShowAgain.isChecked());
+            }
+        });
+
+        //Only show if they have not checked "Do not show again" already
+        if(!Load.loadParserErrorDoNotShow(context)){
+            builder.show();
+        }
     }
 }
