eeeb16d2ff725598b54c6a843bd5a15b29247e11
Julien Guerinet <julien@guerinet.com>
2014-10-11 09:52:33 -0400

    Pressing back on a WebView function now goes back to the previous page is possible.

--- McGill Mobile/src/ca/appvelopers/mcgillmobile/activity/DesktopActivity.java
+++ McGill Mobile/src/ca/appvelopers/mcgillmobile/activity/DesktopActivity.java
@@ -19,6 +19,8 @@ import ca.appvelopers.mcgillmobile.view.DialogHelper;
  * Date: 22/01/14, 9:05 PM
  */
 public class DesktopActivity extends DrawerActivity {
+    private WebView mWebView;
+
     @SuppressLint("SetJavaScriptEnabled")
     public void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_desktop);
@@ -32,20 +34,21 @@ public class DesktopActivity extends DrawerActivity {
             return;
         }
 
-        //Get the Webview
-        final WebView webView = (WebView)findViewById(R.id.desktop_webview);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setDisplayZoomControls(false);
+        //Get the WebView
+        mWebView = (WebView)findViewById(R.id.desktop_webview);
+        mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        mWebView.getSettings().setDisplayZoomControls(false);
 
-        webView.loadUrl("https://mymcgill.mcgill.ca/portal/page/portal/Login");
-        webView.setWebViewClient(new WebViewClient() {
+        mWebView.loadUrl("https://mymcgill.mcgill.ca/portal/page/portal/Login");
+        mWebView.setWebViewClient(new WebViewClient() {
 
-        	public void onPageStarted(WebView view, String url, Bitmap favicon){
-        		//view.loadUrl("javascript:(function(){document.write( '<style class=\"hideStuff\" type=\"text/css\">body {display:none;}</style>')});");
-        		view.setVisibility(View.INVISIBLE);
-        	}
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                //view.loadUrl("javascript:(function(){document.write( '<style class=\"hideStuff\" type=\"text/css\">body {display:none;}</style>')});");
+                view.setVisibility(View.INVISIBLE);
+            }
+
             public void onPageFinished(WebView view, String url) {
                 view.loadUrl("javascript:(function(){document.getElementById('username').value='" +
                         Load.loadFullUsername(DesktopActivity.this) + "';" +
@@ -55,5 +58,15 @@ public class DesktopActivity extends DrawerActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(mWebView.canGoBack()){
+            mWebView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
