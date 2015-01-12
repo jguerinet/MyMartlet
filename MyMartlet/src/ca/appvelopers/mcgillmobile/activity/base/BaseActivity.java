ecb2d08ce4e7fc228d374ab105e08a474bf5fe93
Julien Guerinet <julien@guerinet.com>
2015-01-12 20:37:23 +0100

    Updated the support library, extending the new activity class.

--- MyMartlet/src/ca/appvelopers/mcgillmobile/activity/base/BaseActivity.java
+++ MyMartlet/src/ca/appvelopers/mcgillmobile/activity/base/BaseActivity.java
@@ -2,7 +2,7 @@ package ca.appvelopers.mcgillmobile.activity.base;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 
 import java.util.Locale;
 
@@ -12,7 +12,7 @@ import ca.appvelopers.mcgillmobile.App;
  * Author: Julien
  * Date: 2014-03-16 11:42
  */
-public class BaseActivity extends FragmentActivity {
+public class BaseActivity extends ActionBarActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
