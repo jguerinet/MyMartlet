372d267146aca1be505c81389b946706cfb052fc
Julien Guerinet <julien@guerinet.com>
2015-02-08 12:57:49 +0100

    #23: Added the accept cancel buttons when needed.

--- MyMartlet/src/ca/appvelopers/mcgillmobile/activity/AgreementActivity.java
+++ MyMartlet/src/ca/appvelopers/mcgillmobile/activity/AgreementActivity.java
@@ -1,9 +1,13 @@
 package ca.appvelopers.mcgillmobile.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import ca.appvelopers.mcgillmobile.R;
 import ca.appvelopers.mcgillmobile.activity.main.BaseActivity;
+import ca.appvelopers.mcgillmobile.util.Constants;
 
 /**
  * Author: Julien Guerinet
@@ -19,5 +23,30 @@ public class AgreementActivity extends BaseActivity {
 
         setUpToolbar();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Check if we need to display the buttons
+        if(getIntent().getBooleanExtra(Constants.EULA_REQUIRED, false)){
+            LinearLayout layout = (LinearLayout)findViewById(R.id.buttons_container);
+            layout.setVisibility(View.VISIBLE);
+
+            //Accept
+            Button agree = (Button)findViewById(R.id.button_agree);
+            agree.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setResult(RESULT_OK);
+                    finish();
+                }
+            });
+
+            Button decline = (Button)findViewById(R.id.button_decline);
+            decline.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setResult(RESULT_CANCELED);
+                    finish();
+                }
+            });
+        }
     }
 }
