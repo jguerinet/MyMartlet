fe5ad6aa2988bc90162266e35f0b93e8b883580f
Julien Guerinet <julien@guerinet.com>
2015-02-08 12:00:54 +0100

    #23: Added the text to AgreementActivity

--- MyMartlet/src/ca/appvelopers/mcgillmobile/activity/AgreementActivity.java
+++ MyMartlet/src/ca/appvelopers/mcgillmobile/activity/AgreementActivity.java
@@ -16,5 +16,8 @@ public class AgreementActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agreement);
+
+        setUpToolbar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
