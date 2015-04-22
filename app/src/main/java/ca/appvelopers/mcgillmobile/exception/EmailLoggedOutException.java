7b168fbcc228d8e22300649ceb170043cba3a6bc
Julien Guerinet <julien@guerinet.com>
2015-04-21 23:47:10 -0400

    Removed unused Exception

--- app/src/main/java/ca/appvelopers/mcgillmobile/exception/EmailLoggedOutException.java
+++
@@ -1,7 +0,0 @@
-package ca.appvelopers.mcgillmobile.exception;
-
-public class EmailLoggedOutException extends LoggedOutException {
-	public EmailLoggedOutException() {
-		super("User is logged out of Email.");
-	}
-}
