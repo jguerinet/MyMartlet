6f578eb7e9369399da178313c03c8ff2eabd393b
Julien Guerinet <julien@guerinet.com>
2015-11-11 19:38:09 -0500

    Added the sample file for the password file.

---
+++ app/src/main/java/ca/appvelopers/mcgillmobile/util/PasswordsSample.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014-2015 Appvelopers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ca.appvelopers.mcgillmobile.util;
+
+/**
+ * Sample of the passwords class
+ * @author Julien Guerinet
+ * @version 2.0.0
+ * @since 2.0.0
+ */
+public class PasswordsSample {
+    /**
+     * Username to use for basic auth when downloading the config
+     */
+    public static final String CONFIG_USERNAME = "";
+    /**
+     * Password to use for basic auth when downloading the config
+     */
+    public static final String CONFIG_PASSWORD = "";
+    /**
+     * Key to use when instantiating Instabug
+     */
+    public static final String INSTABUG_KEY = "";
+    /**
+     * Twitter Key and secret for when posting to Twitter
+     */
+    public static final String TWITTER_KEY = "";
+    public static final String TWITTER_SECRET = "";
+}
