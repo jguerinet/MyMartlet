8fc6b59189cf5919dacaf79e5a39a080d54b854a
Julien Guerinet <julien@guerinet.com>
2016-02-10 21:25:40 -0500

    Made the place fields protected for Moshi

--- app/src/main/java/ca/appvelopers/mcgillmobile/model/Place.java
+++ app/src/main/java/ca/appvelopers/mcgillmobile/model/Place.java
@@ -31,40 +31,28 @@ public class Place implements Serializable {
     /**
      * The place name
      */
-    private String name;
+    protected String name;
     /**
-     * The place types
+     * The place categories
      */
-    private List<PlaceType> types;
+    protected List<Integer> categories;
     /**
      * The address of this place
      */
-    private String address;
+    protected String address;
     /**
      * The latitude coordinate of this place
      */
-    private double latitude;
+    protected double latitude;
     /**
      * The longitude coordinate of this place
      */
-    private double longitude;
+    protected double longitude;
 
     /**
-     * Default Constructor
-     *
-     * @param name      The name of this place
-     * @param types     The place types
-     * @param address   The address of this place
-     * @param latitude  The latitude coordinate of this place
-     * @param longitude The longitude coordinate of this place
+     * Default Moshi Constructor
      */
-    public Place(String name, String[] types, String address, double latitude, double longitude) {
-        this.name = name;
-        this.types = PlaceType.getTypes(types);
-        this.address = address;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+    protected Place() {}
 
     /* GETTERS */
 
@@ -96,7 +84,8 @@ public class Place implements Serializable {
      * @return True if it is part of the type, false otherwise
      */
     public boolean isOfType(PlaceType type) {
-        return types.contains(type);
+        return false;
+//        return types.contains(type);
     }
 
     @Override
