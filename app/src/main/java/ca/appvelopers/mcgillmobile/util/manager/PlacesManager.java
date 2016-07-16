9f1814017bb24a1d2d6469f1c6553700329fa33f
Julien Guerinet <julien@guerinet.com>
2016-07-16 17:12:36 -0400

    Fixed bug where removing a favorite place by using an Id was using the Id as the place index.

--- app/src/main/java/ca/appvelopers/mcgillmobile/util/manager/PlacesManager.java
+++ app/src/main/java/ca/appvelopers/mcgillmobile/util/manager/PlacesManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 Appvelopers
+ * Copyright 2014-2016 Julien Guerinet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -160,7 +160,7 @@ public class PlacesManager {
      * @param place {@link Place} to remove from the favorites
      */
     public void removeFavorite(Place place) {
-        getFavoritePlaceIds().remove(place.getId());
+        getFavoritePlaceIds().remove(Integer.valueOf(place.getId()));
         StorageUtils.saveObject(context, getFavoritePlaceIds(), FAVORITE_PLACES, "Favorite Places");
     }
 
