6449a1bbe6736fc76c9936580a2dc24d59c05ead
Julien Guerinet <julien@guerinet.com>
2017-03-02 20:56:36 -0500

    Cleaned up the walkthrough

--- app/src/main/java/ca/appvelopers/mcgillmobile/ui/walkthrough/WalkthroughAdapter.java
+++ app/src/main/java/ca/appvelopers/mcgillmobile/ui/walkthrough/WalkthroughAdapter.java
@@ -20,12 +20,12 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.guerinet.formgenerator.FormGenerator;
-import com.guerinet.formgenerator.TextViewFormItem;
 import com.guerinet.utils.dialog.DialogUtils;
 
 import javax.inject.Inject;
@@ -44,132 +44,128 @@ import ca.appvelopers.mcgillmobile.util.manager.HomepageManager;
  */
 public class WalkthroughAdapter extends PagerAdapter {
     /**
+     * App context
+     */
+    private final Context context;
+    /**
      * True if this is the first open, false otherwise
      *  For a first open there is an extra page at the end
      */
-    private boolean firstOpen;
-    /**
-     * App context
-     */
-    @Inject
-    protected Context context;
+    private final boolean firstOpen;
     /**
      * {@link Analytics} instance
      */
     @Inject
-    protected Analytics analytics;
+    Analytics analytics;
     /**
-     * The {@link HomepageManager} instance
+     * {@link HomepageManager} instance
      */
     @Inject
-    protected HomepageManager homepageManager;
+    HomepageManager homepageManager;
 
-
-    public WalkthroughAdapter(Context context, boolean firstOpen) {
+    /**
+     * Default Constructor
+     *
+     * @param context   App context
+     * @param firstOpen True if this is the first open, false otherwise
+     */
+    WalkthroughAdapter(Context context, boolean firstOpen) {
         super();
+        this.context = context;
         this.firstOpen = firstOpen;
         App.component(context).inject(this);
     }
 
     @Override
     public Object instantiateItem(ViewGroup collection, int position) {
-        final Context context = collection.getContext();
-        View view;
-        switch(position) {
-            //Welcome
+        int layout = 0;
+        switch (position) {
+            // Welcome
             case 0:
-                view = View.inflate(context, R.layout.item_walkthrough_0, null);
+                layout = R.layout.item_walkthrough_0;
                 break;
-            //Access Essentials
+            // Access Essentials
             case 1:
-                view = View.inflate(context, R.layout.item_walkthrough_1, null);
+                layout = R.layout.item_walkthrough_1;
                 break;
-            //Main Menu Explanation
+            // Main Menu Explanation
             case 2:
-                view = View.inflate(context, R.layout.item_walkthrough_2, null);
+                layout = R.layout.item_walkthrough_2;
                 break;
-//            //Horizontal Schedule
+            // Horizontal Schedule
 //            case 3:
-//                view = View.inflate(context, R.layout.item_walkthrough_3, null);
+//                layout = R.layout.item_walkthrough_3;
 //                break;
-            //Info
+            // Info
             case 3:
-                view = View.inflate(context, R.layout.item_walkthrough_4, null);
+                layout = R.layout.item_walkthrough_4;
                 break;
-            //Default HomepageManager / Faculty (first open only)
+            // Default HomepageManager / Faculty (first open only)
             case 4:
-                view = View.inflate(context, R.layout.item_walkthrough_5, null);
-
-                FormGenerator fg = FormGenerator.bind(context, (LinearLayout) view);
-
-                //HomepageManager Prompt
-                fg.text(R.string.walkthrough_homepage)
-                        .gravity(Gravity.CENTER)
-                        .padding(context.getResources()
-                                .getDimensionPixelOffset(R.dimen.padding_small))
-                        .build();
-
-                //HomepageManager
-                fg.text(homepageManager.getTitleString())
-                        .leftIcon(R.drawable.ic_phone_android)
-                        .rightIcon(R.drawable.ic_chevron_right, Color.GRAY)
-                        .onClick(new TextViewFormItem.OnClickListener() {
-                            @Override
-                            public void onClick(final TextViewFormItem item) {
-                                DialogUtils.list(context, R.string.settings_homepage_title,
-                                        new HomepagesAdapter(context) {
-                                            @Override
-                                            public void onHomepageSelected(
-                                                    @HomepageManager.Homepage int choice) {
-                                                //Update it
-                                                homepageManager.set(choice);
-
-                                                item.view().setText(
-                                                        homepageManager.getTitleString());
-
-                                                analytics.sendEvent("Walkthrough",
-                                                        "HomepageManager",
-                                                        homepageManager.getString());
-                                            }
-                                        });
-                            }
-                        })
-                        .build();
-
-                //Faculty Prompt
-                fg.text(R.string.walkthrough_faculty)
-                        .gravity(Gravity.CENTER)
-                        .padding(context.getResources()
-                                .getDimensionPixelOffset(R.dimen.padding_small))
-                        .build();
-
-                //Faculty
-                fg.text(R.string.faculty_none)
-                        .leftIcon(R.drawable.ic_mycourses)
-                        .rightIcon(R.drawable.ic_chevron_right, Color.GRAY)
-                        .onClick(new TextViewFormItem.OnClickListener() {
-                            @Override
-                            public void onClick(final TextViewFormItem item) {
-                                DialogUtils.list(context, R.string.faculty_title,
-                                        new FacultyListAdapter(context,
-                                                item.view().getText().toString()) {
-                                            @Override
-                                            public void onFacultySelected(String faculty) {
-                                                //Update the view
-                                                item.view().setText(faculty);
-
-                                                //If the faculty is not empty, send the GA
-                                                analytics.sendEvent("Walkthrough", "Faculty",
-                                                        faculty);
-                                            }
-                                        });
-                            }
-                        })
-                        .build();
-
                 break;
             default:
                 throw new IllegalStateException("Unknown position " + position + " in walkthrough");
+        }
+
+        View view;
+
+        if (position == 4) {
+            // Set up the question page
+            LinearLayout container = new LinearLayout(context);
+            container.setOrientation(LinearLayout.VERTICAL);
+            container.setGravity(Gravity.CENTER);
+
+            FormGenerator fg = FormGenerator.bind(context, container);
+
+            // HomepageManager Prompt
+            fg.text(R.string.walkthrough_homepage)
+                    .gravity(Gravity.CENTER)
+                    .padding(context.getResources().getDimensionPixelOffset(R.dimen.padding_small))
+                    .build();
+
+            // HomepageManager
+            fg.text(homepageManager.getTitleString())
+                    .leftIcon(R.drawable.ic_phone_android)
+                    .rightIcon(R.drawable.ic_chevron_right, Color.GRAY)
+                    .onClick(item -> DialogUtils.list(context, R.string.settings_homepage_title,
+                            new HomepagesAdapter(context) {
+                                @Override
+                                public void onHomepageSelected(@HomepageManager.Homepage
+                                        int choice) {
+                                    // Update it
+                                    homepageManager.set(choice);
+
+                                    item.view().setText(homepageManager.getTitleString());
+
+                                    analytics.sendEvent("Walkthrough", "HomepageManager",
+                                            homepageManager.getString());
+                                }
+                            }))
+                    .build();
+
+            // Faculty Prompt
+            fg.text(R.string.walkthrough_faculty)
+                    .gravity(Gravity.CENTER)
+                    .padding(context.getResources().getDimensionPixelOffset(R.dimen.padding_small))
+                    .build();
+
+            // Faculty
+            fg.text(R.string.faculty_none)
+                    .leftIcon(R.drawable.ic_mycourses)
+                    .rightIcon(R.drawable.ic_chevron_right, Color.GRAY)
+                    .onClick(item -> DialogUtils.list(context, R.string.faculty_title,
+                            new FacultyListAdapter(context, item.view().getText().toString()) {
+                                @Override
+                                public void onFacultySelected(String faculty) {
+                                    // Update the view
+                                    item.view().setText(faculty);
+                                    analytics.sendEvent("Walkthrough", "Faculty", faculty);
+                                }
+                            }))
+                    .build();
+            view = container;
+        } else {
+            view = LayoutInflater.from(context).inflate(layout, null);
         }
 
         collection.addView(view);
