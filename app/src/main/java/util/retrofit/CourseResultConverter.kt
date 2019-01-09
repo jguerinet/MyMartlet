247392f4278f63190aa7df72310b93c61892f268
Allan Wang <me@allanwang.ca>
2019-01-08 21:33:56 -0500

    Rewrite course result converter
    For the most part, unnecessary code added through automatic conversions were removed.
    There are also the following changes:
    * Code is divided into blocks
    * Self is returned from the factory rather than a new converter
    * Time parsing is based around a regex expression (untested)
    * Integer and double parsing are done the kotlin way, and exceptions are caught within the convert function
    * Immutable lists are used
    
    Ideally I think all parsing logic should be extracted as objects and also tested.

--- app/src/main/java/util/retrofit/CourseResultConverter.kt
+++ app/src/main/java/util/retrofit/CourseResultConverter.kt
@@ -16,13 +16,13 @@
 
 package com.guerinet.mymartlet.util.retrofit
 
-import androidx.core.util.Pair
 import com.guerinet.mymartlet.model.CourseResult
 import com.guerinet.mymartlet.model.Term
 import com.guerinet.mymartlet.util.DayUtils
 import com.squareup.moshi.Types
 import okhttp3.ResponseBody
 import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 import org.threeten.bp.DayOfWeek
 import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalTime
@@ -32,7 +32,6 @@ import timber.log.Timber
 import java.io.IOException
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
-import java.util.*
 
 /**
  * Retrofit converter to parse a list of course results when searching for courses
@@ -44,22 +43,142 @@ class CourseResultConverter : Converter.Factory(), Converter<ResponseBody, List<
     /** [ParameterizedType] representing a list of [CourseResult]s */
     private val type = Types.newParameterizedType(List::class.java, CourseResult::class.java)
 
+    /**
+     * Returns self assuming that the type matches
+     */
     override fun responseBodyConverter(
-        type: Type?, annotations: Array<Annotation>?,
-        retrofit: Retrofit?
-    ): Converter<ResponseBody, *>? {
-        return if (type?.toString() != this.type.toString()) {
-            //This can only convert a list of course results
-            null
-        } else CourseResultConverter()
+        type: Type, annotations: Array<Annotation>,
+        retrofit: Retrofit
+    ): Converter<ResponseBody, *>? = takeIf {
+        type.toString() != this.type.toString()
+    }
+
+    private val timeParser = Regex("([0-9]+):([0-9]+) (am|pm)", RegexOption.IGNORE_CASE)
+
+    private fun String.getTime(): LocalTime? {
+        val matches = timeParser.find(this)?.groupValues ?: return null
+        var hour = matches[0].toInt()
+        val minute = matches[1].toInt()
+        val pm = matches[2] == "pm"
+        // If it's PM, then add 12 hours to the hours for 24 hours format
+        // Make sure it isn't noon
+        // We can ignore midnight as there are no classes then
+        if (pm && hour != 12) {
+            hour += 12
+        }
+        return LocalTime.of(hour, minute)
+    }
+
+    private fun Elements.parseCourse(term: Term): CourseResult? {
+        if (size < 19)
+        // Not enough elements
+            return null
+
+        // Create a new course object with the default values
+        var credits = 99.0
+        var subject: String? = null
+        var number: String? = null
+        var title = ""
+        var type = ""
+        var days: List<DayOfWeek> = emptyList()
+        var crn = 0
+        var instructor = ""
+        var location = ""
+        //So that the rounded start time will be 0
+        var startTime = ScheduleConverter.defaultStartTime
+        var endTime = ScheduleConverter.defaultEndTime
+        var capacity = 0
+        var seatsRemaining = 0
+        var waitlistRemaining = 0
+        var startDate = LocalDate.now()
+        var endDate = LocalDate.now()
+
+        // TODO note that the only cause of failure here is now the DayUtils conversion
+        try {
+            // Note that a while loop is necessary as the list may be modified during the loop
+            var i = 0
+            while (i < size) {
+                val element = getOrNull(i) ?: break
+                if (element.toString().contains("&nbsp;")) {
+                    // Empty row: continue
+                    i++
+                    continue
+                }
+
+                val rowString: String = element.text()
+
+                when (i) {
+                    // CRN
+                    1 -> crn = rowString.toIntOrNull() ?: return null
+                    // Subject
+                    2 -> subject = rowString
+                    // Number
+                    3 -> number = rowString
+                    // Type
+                    5 -> type = rowString
+                    // Number of credits
+                    6 -> credits = rowString.toDoubleOrNull() ?: return null
+                    // Course title
+                    7 -> title = rowString.trimEnd('.')
+                    // Days of the week
+                    8 -> if (rowString == "TBA") {
+                        // TBA Stuff: no time associated so skip the next one
+                        // and add a dummy to keep the index correct
+                        add(9, null)
+                        i++
+                    } else {
+                        // Day Parsing
+                        days = rowString.replace('\u00A0', ' ').trim().map { DayUtils.getDay(it) }
+                    }
+                    // Time
+                    9 -> {
+                        val times =
+                            rowString.split("-", limit = 3)
+
+                        startTime = times.getOrNull(0)?.getTime() ?: startTime
+                        endTime = times.getOrNull(0)?.getTime() ?: endTime
+                    }
+                    // Capacity
+                    10 -> capacity = rowString.toIntOrNull() ?: return null
+                    // Seats remaining
+                    12 -> seatsRemaining = rowString.toIntOrNull() ?: return null
+                    // Waitlist remaining
+                    15 -> waitlistRemaining = rowString.toIntOrNull() ?: return null
+                    // Instructor
+                    16 -> instructor = rowString
+                    // Start/end date
+                    17 -> {
+                        val dates = parseDateRange(term, rowString)
+                        startDate = dates.first
+                        endDate = dates.second
+                    }
+                    // Location
+                    18 -> location = rowString
+                }
+                i++
+            }
+        } catch (e: Exception) {
+            Timber.e(e, "Course Results Parser Error")
+            return null
+        }
+
+        // Validate course
+        if (subject == null || number == null)
+            return null
+
+        return CourseResult(
+            term, subject, number, title, crn, "", startTime,
+            endTime, days, type, location, instructor, credits, startDate, endDate,
+            capacity, seatsRemaining, waitlistRemaining
+        )
     }
 
     @Throws(IOException::class)
     override fun convert(value: ResponseBody): List<CourseResult> {
         val html = value.string()
-        val courses = ArrayList<CourseResult>()
         val document = Jsoup.parse(html, "UTF-8")
         //Parse the response body into a list of rows
+        // todo unused
         val rows = document.getElementsByClass("dddefault")
 
         // Parse the currentTerm from the page header
@@ -70,177 +189,24 @@ class CourseResultConverter : Converter.Factory(), Converter<ResponseBody, List<
         val table = document.getElementsByClass("datadisplaytable")[0].select("tbody")[0]
 
         // Go through the rows in the table
-        for (row in table.select("tr")) {
-            // Check that there at least 19 elements in the row
-            val rowElements = row.select("td")
-            if (rowElements.size < 19) {
-                // If there aren't, it must not be a course row
-                continue
-            }
-
-            // Create a new course object with the default values
-            var credits = 99.0
-            var subject: String? = null
-            var number: String? = null
-            var title = ""
-            var type = ""
-            val days = ArrayList<DayOfWeek>()
-            var crn = 0
-            var instructor = ""
-            var location = ""
-            //So that the rounded start time will be 0
-            var startTime = ScheduleConverter.defaultStartTime
-            var endTime = ScheduleConverter.defaultEndTime
-            var capacity = 0
-            var seatsRemaining = 0
-            var waitlistRemaining = 0
-            var startDate: LocalDate? = LocalDate.now()
-            var endDate: LocalDate? = LocalDate.now()
-
-            try {
-                var i = 0
-                while (i < rowElements.size) {
-                    if (rowElements[i].toString().contains("&nbsp;")) {
-                        // Empty row: continue
-                        i++
-                        continue
-                    }
-                    var rowString = rowElements[i].text()
-
-                    when (i) {
-                        // CRN
-                        1 -> crn = Integer.parseInt(rowString)
-                        // Subject
-                        2 -> subject = rowString
-                        // Number
-                        3 -> number = rowString
-                        // Type
-                        5 -> type = rowString
-                        // Number of credits
-                        6 -> credits = java.lang.Double.parseDouble(rowString)
-                        // Course title
-                        7 ->
-                            //Remove the extra period at the end of the course title
-                            title = rowString.substring(0, rowString.length - 1)
-                        // Days of the week
-                        8 -> if (rowString == "TBA") {
-                            // TBA Stuff: no time associated so skip the next one
-                            // and add a dummy to keep the index correct
-                            rowElements.add(9, null)
-                            i++
-                        } else {
-                            // Day Parsing
-                            rowString = rowString.replace('\u00A0', ' ').trim { it <= ' ' }
-                            for (k in 0 until rowString.length) {
-                                days.add(DayUtils.getDay(rowString[k]))
-                            }
-                        }
-                        // Time
-                        9 -> {
-                            val times =
-                                rowString.split("-".toRegex()).dropLastWhile { it.isEmpty() }
-                                    .toTypedArray()
-                            try {
-                                var startHour =
-                                    Integer.parseInt(
-                                        times[0].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0].split(
-                                            ":".toRegex()
-                                        ).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                                    )
-                                val startMinute =
-                                    Integer.parseInt(
-                                        times[0].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0].split(
-                                            ":".toRegex()
-                                        ).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                                    )
-                                var endHour =
-                                    Integer.parseInt(
-                                        times[1].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0].split(
-                                            ":".toRegex()
-                                        ).dropLastWhile { it.isEmpty() }.toTypedArray()[0]
-                                    )
-                                val endMinute =
-                                    Integer.parseInt(
-                                        times[1].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[0].split(
-                                            ":".toRegex()
-                                        ).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                                    )
-
-                                //If it's PM, then add 12 hours to the hours for 24 hours format
-                                //Make sure it isn't noon
-                                val startPM =
-                                    times[0].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                                if (startPM == "PM" && startHour != 12) {
-                                    startHour += 12
-                                }
-
-                                val endPM =
-                                    times[1].split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()[1]
-                                if (endPM == "PM" && endHour != 12) {
-                                    endHour += 12
-                                }
-
-                                startTime = LocalTime.of(startHour, startMinute)
-                                endTime = LocalTime.of(endHour, endMinute)
-                            } catch (e: NumberFormatException) {
-                                //Courses sometimes don't have assigned times
-                                startTime = ScheduleConverter.defaultStartTime
-                                endTime = ScheduleConverter.defaultEndTime
-                            }
-
-                        }
-                        // Capacity
-                        10 -> capacity = Integer.parseInt(rowString)
-                        // Seats remaining
-                        12 -> seatsRemaining = Integer.parseInt(rowString)
-                        // Waitlist remaining
-                        15 -> waitlistRemaining = Integer.parseInt(rowString)
-                        // Instructor
-                        16 -> instructor = rowString
-                        // Start/end date
-                        17 -> {
-                            val dates = parseDateRange(term, rowString)
-                            startDate = dates.first
-                            endDate = dates.second
-                        }
-                        // Location
-                        18 -> location = rowString
-                    }
-                    i++
-                }
-            } catch (e: Exception) {
-                Timber.e(e, "Course Results Parser Error")
-            }
-
-            // Don't add any courses with errors
-            if (subject != null && number != null) {
-                // Create a new course object and add it to list
-                // TODO Should we be parsing the course section?
-                courses.add(
-                    CourseResult(
-                        term, subject, number, title, crn, "", startTime,
-                        endTime, days, type, location, instructor, credits, startDate!!, endDate!!,
-                        capacity, seatsRemaining, waitlistRemaining
-                    )
-                )
-            }
-        }
-
-        return courses
+        return table.select("tr").map { it.select("td") }.mapNotNull { it.parseCourse(term) }
     }
 
     /**
      * Parses a String into a LocalDate object
      *
+     * TODO should just use a regex and return null if not found?
+     *
      * @param term Current currentTerm
      * @param date The date String
      * @return The corresponding local date
      */
+    @Throws(Exception::class)
     fun parseDate(term: Term, date: String): LocalDate {
-        val dateFields = date.split("/".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val (month, day) = date.split("/")
         return LocalDate.of(
-            term.year, Integer.parseInt(dateFields[0]),
-            Integer.parseInt(dateFields[1])
+            term.year, month.toInt(),
+            day.toInt()
         )
     }
 
@@ -255,11 +221,11 @@ class CourseResultConverter : Converter.Factory(), Converter<ResponseBody, List<
     @Throws(IllegalArgumentException::class)
     fun parseDateRange(term: Term, dateRange: String): Pair<LocalDate, LocalDate> {
         //Split the range into the 2 date Strings
-        val dates = dateRange.split("-".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-        val startDate = dates[0].trim { it <= ' ' }
-        val endDate = dates[1].trim { it <= ' ' }
+        val dates = dateRange.split("-")
+        val startDate = dates[0].trim()
+        val endDate = dates[1].trim()
 
         //Parse the dates, return them as a pair
-        return Pair(parseDate(term, startDate), parseDate(term, endDate))
+        return parseDate(term, startDate) to parseDate(term, endDate)
     }
 }
