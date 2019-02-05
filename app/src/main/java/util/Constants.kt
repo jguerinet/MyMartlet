136b60aaaebec04d0896fb70d8853542659b4b19
Julien Guerinet <julien@guerinet.com>
2019-02-05 06:34:55 -0500

    Add Firebase Category db name to the Constants

--- app/src/main/java/util/Constants.kt
+++ app/src/main/java/util/Constants.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018 Julien Guerinet
+ * Copyright 2014-2019 Julien Guerinet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,6 +38,14 @@ object Constants {
     /* BROADCASTS */
 
     const val BROADCAST_MINERVA = "broadcast_minerva"
+
+    /**
+     * Db names within Firebase
+     */
+    object Firebase {
+
+        const val CATEGORIES = "categories"
+    }
 }
 
 /**
