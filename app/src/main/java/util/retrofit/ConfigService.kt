506cdf4287698649550247f29e0e54379a548fef
Julien Guerinet <julien@guerinet.com>
2019-02-08 13:19:59 -0500

    Remove old ConfigService functions

--- app/src/main/java/util/retrofit/ConfigService.kt
+++ app/src/main/java/util/retrofit/ConfigService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2018 Julien Guerinet
+ * Copyright 2014-2019 Julien Guerinet
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,6 @@
 package com.guerinet.mymartlet.util.retrofit
 
 import com.guerinet.mymartlet.model.Term
-import com.guerinet.mymartlet.model.place.Category
-import com.guerinet.mymartlet.model.place.Place
 import com.guerinet.mymartlet.util.service.ConfigDownloadService
 import retrofit2.Call
 import retrofit2.http.GET
@@ -36,18 +34,6 @@ interface ConfigService {
      */
     @GET("config")
     fun config(@Header("If-Modified-Since") ims: String?): Call<ConfigDownloadService.Config>
-
-    /**
-     * Retrieves the list of [Place]. Uses the [ims] header
-     */
-    @GET("places")
-    fun places(@Header("If-Modified-Since") ims: String?): Call<List<Place>>
-
-    /**
-     * Retrieves the list of [Category]s. Uses the [ims] header
-     */
-    @GET("categories")
-    fun categories(@Header("If-Modified-Since") ims: String?): Call<List<Category>>
 
     /**
      * Retrieves the list of [Term]s one can currently register for. Uses the [ims] header
